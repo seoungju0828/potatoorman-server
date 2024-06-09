15333524df10a36ab8d6fc3403ef6858788fc719
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-06-09 16:32:30 +0900

    fix: 포트번호 변경

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.use('/storyApi', storyRoute)    // 스토리 대사
 app.use('/gaugeInfoApi', gaugeInfoRoute) // 게이지 정보 저장 및 결과창으로 정보 전달
 // app.use('/pictureApi', pictureRoute) // 갤러리 사진 저장 + 이메일 전송
 
-app.listen(8080, function () {
+app.listen(8081, function () {
     console.log('success!')
 })
