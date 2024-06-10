dda191c6631b4f045904ae40e307afea8c15280c
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-06-11 03:18:07 +0900

    feat: fianlScore 게이지 화면에 출력

--- routes/gaugeInfo.js
+++ routes/gaugeInfo.js
@@ -9,7 +9,7 @@ router.post('/:roundIdx', (req, res) => {
     const { counter } = req.body
     const userId = 1
 
-    // console.log(roundIdx)
+    console.log(roundIdx)
 
     let score = '';
     switch (roundIdx) {
@@ -27,7 +27,9 @@ router.post('/:roundIdx', (req, res) => {
             break
     }
 
-    const query = `update user set ${score} = ${counter} where id = ${userId}`
+    console.log(score)
+    const query = `UPDATE user SET ${score} = ${counter} WHERE id = ${userId}`
+
     db.query(query, (err, result) => {
         if (err) {
             console.error('Error updating scores:', err)
@@ -63,7 +65,7 @@ router.get('/result', (req, res) => {
     db.query(query, (err, results) => {
         if (err) {
             console.error('Error retrieving scores:', err)
-            return res.status(500).json({ error: 'Error retrieving scores' })
+            res.status(500).json({ error: 'Error retrieving scores' })
         } else {
             if (results.length > 0) {
                 const scores = {
@@ -72,27 +74,13 @@ router.get('/result', (req, res) => {
                     thirdScore: results[0].thirdScore,
                     finalScore: results[0].finalScore
                 }
-
-                let changeQuery = ''
-                if (scores.finalScore >= 80) changeQuery = `update user set changeOX='O' where id=${userId}`
-                else changeQuery = `update user set changeOX='X' where id=${userId}`
-
-                db.query(changeQuery, (err, updateResults) => {
-                    if (err) {
-                        console.error('Error updating finalScore:', err)
-                        return res.status(500).json({ error: 'Error updating finalScore' })
-                    }
-                    res.status(200).json({
-                        scores: scores,
-                        message: 'Scores and finalScore updated successfully'
-                    })
-                })
-
+                res.status(200).json(scores)
             } else {
                 res.status(404).json({ error: 'Scores not found' })
             }
         }
     })
+
 })
 
 module.exports = router
