16241284a9f3da62de956e8a9df2f5b3881adf42
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-06-10 14:54:07 +0900

    feat: finalScore 값 저장

--- routes/gaugeInfo.js
+++ routes/gaugeInfo.js
@@ -9,7 +9,7 @@ router.post('/:roundIdx', (req, res) => {
     const { counter } = req.body
     const userId = 1
 
-    console.log(roundIdx)
+    // console.log(roundIdx)
 
     let score = '';
     switch (roundIdx) {
@@ -27,41 +27,72 @@ router.post('/:roundIdx', (req, res) => {
             break
     }
 
-    console.log(score)
-    const query = `UPDATE user SET ${score} = ${counter} WHERE id = ${userId}`
-
+    const query = `update user set ${score} = ${counter} where id = ${userId}`
     db.query(query, (err, result) => {
         if (err) {
             console.error('Error updating scores:', err)
             res.status(500).json({ error: 'Error updating scores' })
         } else {
-            res.status(200).json({ message: 'Score updated successfully' })
+            const getSumQuery = `select firstScore + secondScore + thirdScore as sum from user where id = ${userId}`
+            db.query(getSumQuery, (err, results) => {
+                if (err) {
+                    console.error('Error fetching sum:', err)
+                    return res.status(500).json({ error: 'Error fetching sum' })
+                }
+
+                const sum = results[0].sum / 3
+
+                const finalScoreQuery = `update user set finalScore = ${sum} where id = ${userId}`
+                db.query(finalScoreQuery, (err, result) => {
+                    if (err) {
+                        console.error('Error updating finalScore:', err)
+                        return res.status(500).json({ error: 'Error updating finalScore' })
+                    }
+
+                    res.status(200).json({ message: 'Scores and finalScore updated successfully' })
+                })
+            })
         }
     })
 })
 
 router.get('/result', (req, res) => {
     const userId = 1
-    const query = `select firstScore, secondScore, thirdScore from user where id = ${userId}`
+    const query = `select firstScore, secondScore, thirdScore, finalScore from user where id = ${userId}`
 
     db.query(query, (err, results) => {
         if (err) {
             console.error('Error retrieving scores:', err)
-            res.status(500).json({ error: 'Error retrieving scores' })
+            return res.status(500).json({ error: 'Error retrieving scores' })
         } else {
             if (results.length > 0) {
                 const scores = {
                     firstScore: results[0].firstScore,
                     secondScore: results[0].secondScore,
-                    thirdScore: results[0].thirdScore
+                    thirdScore: results[0].thirdScore,
+                    finalScore: results[0].finalScore
                 }
-                res.status(200).json(scores)
+
+                let changeQuery = ''
+                if (scores.finalScore >= 80) changeQuery = `update user set changeOX='O' where id=${userId}`
+                else changeQuery = `update user set changeOX='X' where id=${userId}`
+
+                db.query(changeQuery, (err, updateResults) => {
+                    if (err) {
+                        console.error('Error updating finalScore:', err)
+                        return res.status(500).json({ error: 'Error updating finalScore' })
+                    }
+                    res.status(200).json({
+                        scores: scores,
+                        message: 'Scores and finalScore updated successfully'
+                    })
+                })
+
             } else {
                 res.status(404).json({ error: 'Scores not found' })
             }
         }
     })
-
 })
 
 module.exports = router
